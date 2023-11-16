85103ef35721d5afdd36343ff5cc6a7f4aea3d93
Tom Brauer <tombrauer0102@gmail.com>
2023-11-17 00:03:18 +0100

    tree first version

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args)
+    {
+        int height = getHeight();
+        printTree(height);
+    }
+    private static int getHeight() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Hoehe des Baumes");
+        System.out.print(getInputSymbol());
+
+        int num = scan.nextInt();
+        scan.close();
+        return num;
+    }
+
+    private static String getInputSymbol() {
+        return "> ";
+    }
+
+    private static void printTree(int height) {
+        StringBuilder fillWhitespace = new StringBuilder();
+        while(fillWhitespace.length() < (height * 2) - 4) {
+            fillWhitespace.append(" ");
+        }
+
+        StringBuilder fillUnderscore = new StringBuilder();
+        while(fillUnderscore.length() < (height - 1) * 2) {
+            fillUnderscore.append("_");
+        }
+
+        for (int i = 0; i < height - 1; i++) {
+            System.out.println(fillWhitespace.substring(i, height - 1) + "/" + fillWhitespace.substring(0, i * 2) + "\\");
+        }
+        System.out.println("/" + fillUnderscore + "\\");
+        System.out.println(fillWhitespace.substring(0, fillUnderscore.length() / 2) + "||");
+    }
+
+}
